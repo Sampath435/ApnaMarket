adf5722c26426eece22400cbe58eb9872de032ec
Sampath_Dev <sampathmotapandle@gmail.com>
2026-02-05 14:44:01 +0530

    added java backend files

---
+++ Backend/src/main/java/com/Backend/ApnaMarket/ApnaMarketApplication.java
@@ -0,0 +1,13 @@
+package com.Backend.ApnaMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApnaMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApnaMarketApplication.class, args);
+	}
+
+}
---
+++ Backend/src/main/java/com/Backend/ApnaMarket/model/Product.java
@@ -0,0 +1,35 @@
+package com.Backend.ApnaMarket.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String desc;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+    private Date releaseDate;
+    private boolean available;
+    private int stockQuantity;
+
+    private String imageName;
+    private String imageType;
+    @Lob
+    private byte[] imageData;
+}
+
